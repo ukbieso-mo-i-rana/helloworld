98352ccc65237d9db3e15937b8a69e6d015bd2d2
Eier <Eier@Eier-HP>
2017-05-20 22:13:57 +0200

    here another additions

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -2,6 +2,7 @@
 public class HelloUkbieso {
 
 	public static void main(String[] args) {
+		String added ="here another addition";
 		// this is the branch with comment
 		String myname ="thirdname of GIT";
 		System.out.println("in second featur added functionality!");
