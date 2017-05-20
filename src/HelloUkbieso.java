a432dd38bf9d790db9a4663c3de8e45d5a1acbd4
Eier <Eier@Eier-HP>
2017-05-20 22:27:41 +0200

    edited

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -2,8 +2,6 @@
 public class HelloUkbieso {
 
 	public static void main(String[] args) {
-		
-		
 		String myname ="we love summer";
 		System.out.println("in second featur added functionality!");
 		// add some good functionalities to the master local 
