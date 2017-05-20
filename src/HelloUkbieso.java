7540c3504316210df5353142ef1351b8dcc07b63
Eier <Eier@Eier-HP>
2017-05-20 22:00:00 +0200

    new name variable added to master

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -3,7 +3,9 @@ public class HelloUkbieso {
 
 	public static void main(String[] args) {
 		// this is the branch with comment
+		String myname ="git";
 		// add some good functionalities to the master local 
+		
 		System.out.println("hello all GitHub user....... it is do nice to see folk contuributing to opensorce code!");
 	}
 
