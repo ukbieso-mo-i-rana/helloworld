fb0ac6ccd27b85f7e4693ac4f4c1596af0c8740d
Eier <Eier@Eier-HP>
2017-05-20 22:24:21 +0200

    added string asigned null

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -2,8 +2,8 @@
 public class HelloUkbieso {
 
 	public static void main(String[] args) {
-		String added ="here another addition";
-		// this is the branch with comment
+		String added =null;
+		
 		String myname ="thirdname of GIT";
 		System.out.println("in second featur added functionality!");
 		// add some good functionalities to the master local 
