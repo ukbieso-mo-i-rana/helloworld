322c4a3bd93733c1f0852f7e5d0e962e31b661c4
Eier <Eier@Eier-HP>
2017-05-20 22:02:43 +0200

    third name git added to the third branch

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -3,7 +3,7 @@ public class HelloUkbieso {
 
 	public static void main(String[] args) {
 		// this is the branch with comment
-		String myname ="git";
+		String myname ="thirdname of GIT";
 		// add some good functionalities to the master local 
 		
 		System.out.println("hello all GitHub user....... it is do nice to see folk contuributing to opensorce code!");
