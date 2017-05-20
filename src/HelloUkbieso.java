0eab3b11cf59145d84fd0e3c495e666442eee95e
Eier <Eier@Eier-HP>
2017-05-20 22:06:58 +0200

    second feature functionlity added system.out.print

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -3,6 +3,7 @@ public class HelloUkbieso {
 
 	public static void main(String[] args) {
 		// this is the branch with comment
+		System.out.println("in second featur added functionality!");
 		// add some good functionalities to the master local 
 		System.out.println("hello all GitHub user....... it is do nice to see folk contuributing to opensorce code!");
 	}
