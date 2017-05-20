40cd458ded273326213a421d05b98d0181a97d83
Eier <Eier@Eier-HP>
2017-05-20 21:48:13 +0200

    this is my feature file commited to local

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -2,7 +2,7 @@
 public class HelloUkbieso {
 
 	public static void main(String[] args) {
-		
+		// this is the branch with comment
 		System.out.println("hello all GitHub user....... it is do nice to see folk contuributing to opensorce code!");
 	}
 
