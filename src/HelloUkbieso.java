a3ed5b03e451f2a1ae683de4a5cc61f7911b9572
Eier <Eier@Eier-HP>
2017-05-20 21:49:47 +0200

    added functionlities to master

--- src/HelloUkbieso.java
+++ src/HelloUkbieso.java
@@ -2,7 +2,7 @@
 public class HelloUkbieso {
 
 	public static void main(String[] args) {
-		
+		// add some good functionalities to the master local
 		System.out.println("hello all GitHub user....... it is do nice to see folk contuributing to opensorce code!");
 	}
 
